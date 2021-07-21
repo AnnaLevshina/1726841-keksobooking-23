5e65a6a03502579c412616f907c3b9c4b6a05c6b
Anna <AnnaLevshina@users.noreply.github.com>
2021-07-21 14:34:38 +0300

    module7-task2 #2

--- js/inactive-state.js
+++ js/inactive-state.js
@@ -1,61 +1,61 @@
-function inactive_state() {
+function deactivateForm() {
 
-const formElement =  document.querySelector('.ad-form');
-formElement.classList.add('ad-form--disabled');
+  const formElement =  document.querySelector('.ad-form');
+  formElement.classList.add('ad-form--disabled');
 
-const mapFiltersElement =  document.querySelector('.map__filters');
-mapFiltersElement.classList.add('ad-form--disabled');
+  const mapFiltersElement =  document.querySelector('.map__filters');
+  mapFiltersElement.classList.add('ad-form--disabled');
 
-const fieldsetFilters = document.querySelector('.notice');
-const fieldsets =fieldsetFilters.querySelectorAll('fieldset');
-for (let i =0, length = fieldsets.length; i < length; i++){
-  fieldsets[i].setAttribute("disabled", "disabled");
-};
+  const fieldsetFilters = document.querySelector('.notice');
+  const fieldsets =fieldsetFilters.querySelectorAll('fieldset');
+  for (let iii =0, length = fieldsets.length; iii < length; iii++){  //let i исправила на iii из-за того, что пишет Identifier name 'i' is too short (< 2)
+    fieldsets[iii].setAttribute('disabled', 'disabled');
+  }
 
-const mapFilters = document.querySelector('.map__filters-container');
-const mapFiltersSelect = mapFilters.querySelectorAll('select');
-const mapFiltersFieldset = mapFilters.querySelectorAll('fieldset');
+  const mapFilters = document.querySelector('.map__filters-container');
+  const mapFiltersSelect = mapFilters.querySelectorAll('select');
+  const mapFiltersFieldset = mapFilters.querySelectorAll('fieldset');
 
-for (let i =0, length = mapFiltersSelect.length; i < length; i++){
-  mapFiltersSelect[i].setAttribute("disabled", "disabled");
-};
+  for (let iii =0, length = mapFiltersSelect.length; iii < length; iii++){
+    mapFiltersSelect[iii].setAttribute('disabled', 'disabled');
+  }
 
-for (let i =0, length = mapFiltersFieldset.length; i < length; i++){
-  mapFiltersFieldset[i].setAttribute("disabled", "disabled");
-};
+  for (let iii =0, length = mapFiltersFieldset.length; iii < length; iii++){
+    mapFiltersFieldset[iii].setAttribute('disabled', 'disabled');
+  }
 
-};
+}
 
-inactive_state();
+deactivateForm();
 
 
-function active_state() {
+function activateForm() {
 
-const formElement =  document.querySelector('.ad-form');
-formElement.classList.remove('ad-form--disabled');
+  const formElement =  document.querySelector('.ad-form');
+  formElement.classList.remove('ad-form--disabled');
 
-const mapFiltersElement =  document.querySelector('.map__filters');
-mapFiltersElement.classList.remove('ad-form--disabled');
+  const mapFiltersElement =  document.querySelector('.map__filters');
+  mapFiltersElement.classList.remove('ad-form--disabled');
 
-const fieldsetFilters = document.querySelector('.notice');
-const fieldsets =fieldsetFilters.querySelectorAll('fieldset');
-for (let i =0, length = fieldsets.length; i < length; i++){
-  fieldsets[i].removeAttribute("disabled", "disabled");
-};
+  const fieldsetFilters = document.querySelector('.notice');
+  const fieldsets =fieldsetFilters.querySelectorAll('fieldset');
+  for (let iii =0, length = fieldsets.length; iii < length; iii++){
+    fieldsets[iii].removeAttribute('disabled', 'disabled');
+  }
 
-const mapFilters = document.querySelector('.map__filters-container');
-const mapFiltersSelect = mapFilters.querySelectorAll('select');
-const mapFiltersFieldset = mapFilters.querySelectorAll('fieldset');
+  const mapFilters = document.querySelector('.map__filters-container');
+  const mapFiltersSelect = mapFilters.querySelectorAll('select');
+  const mapFiltersFieldset = mapFilters.querySelectorAll('fieldset');
 
-for (let i =0, length = mapFiltersSelect.length; i < length; i++){
-  mapFiltersSelect[i].removeAttribute("disabled", "disabled");
-};
+  for (let iii =0, length = mapFiltersSelect.length; iii < length; iii++){
+    mapFiltersSelect[iii].removeAttribute('disabled', 'disabled');
+  }
 
-for (let i =0, length = mapFiltersFieldset.length; i < length; i++){
-  mapFiltersFieldset[i].removeAttribute("disabled", "disabled");
-};
+  for (let iii =0, length = mapFiltersFieldset.length; iii < length; iii++){
+    mapFiltersFieldset[iii].removeAttribute('disabled', 'disabled');
+  }
 
-};
+}
 
-/*active_state();
+/*activateForm();
 */
